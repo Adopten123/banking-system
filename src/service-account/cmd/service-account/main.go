0fb363cce48855308633d0a21e1dfd64a1160bfb
Adopten123 <alexandermikhailov0505@gmail.com>
2026-02-19 22:34:04 +0300

    ping handler, testing http-server settings

--- src/service-account/cmd/service-account/main.go
+++ src/service-account/cmd/service-account/main.go
@@ -1,14 +1,52 @@
 package main
 
 import (
-	"fmt"
-	"log/slog"
+	"encoding/json"
+	"log"
+	"net/http"
 	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
-	log := slog.New(slog.NewJSONHandler(os.Stdout, nil))
-	log.Info("Starting Service Account")
+	dbURL := os.Getenv("DB_URL")
+	redisURL := os.Getenv("REDIS_URL")
 
-	fmt.Println("Server is running...")
+	log.Println("Starting service-account...")
+	log.Printf("DB_URL: %s\n", dbURL)
+	log.Printf("REDIS_URL: %s\n", redisURL)
+
+	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/api/ping", pingHandler)
+
+	port := ":8080"
+	log.Printf("Server is listening on port %s\n", port)
+
+	if err := http.ListenAndServe(port, r); err != nil {
+		log.Fatalf("Server failed to start: %v", err)
+	}
+}
+
+func pingHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	response := map[string]string{
+		"service": "account",
+		"status":  "ok",
+		"version": "1.0.0",
+		"router":  "go-chi/v5",
+	}
+
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		log.Printf("Error encoding response: %v", err)
+	}
 }
