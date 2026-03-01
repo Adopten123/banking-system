dfcea21b940417680a8efa1a8204e9d289d0d302
Adopten123 <alexandermikhailov0505@gmail.com>
2026-03-01 16:12:04 +0300

    add events strcuts

---
+++ src/service-notification/internal/domain/events.go
@@ -0,0 +1,36 @@
+package domain
+
+import "time"
+
+type TransferCreatedEvent struct {
+	TransactionID  string    `json:"transaction_id"`
+	FromAccountID  int       `json:"from_account_id"`
+	ToAccountID    int       `json:"to_account_id"`
+	Amount         string    `json:"amount"`
+	Currency       string    `json:"currency"`
+	IdempotencyKey string    `json:"idempotency_key"`
+	Timestamp      time.Time `json:"timestamp"`
+}
+
+type AccountCreatedEvent struct {
+	AccountID int       `json:"account_id"`
+	PublicID  string    `json:"public_id"`
+	UserID    string    `json:"user_id"`
+	Currency  string    `json:"currency"`
+	Timestamp time.Time `json:"timestamp"`
+}
+
+type AccountStatusChangedEvent struct {
+	AccountID int       `json:"account_id"`
+	OldStatus int       `json:"old_status"`
+	NewStatus int       `json:"new_status"`
+	Timestamp time.Time `json:"timestamp"`
+}
+
+type DepositCompletedEvent struct {
+	TransactionID string    `json:"transaction_id"`
+	AccountID     int       `json:"account_id"`
+	Amount        string    `json:"amount"`
+	Currency      string    `json:"currency"`
+	Timestamp     time.Time `json:"timestamp"`
+}
